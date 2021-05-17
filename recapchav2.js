d37fef3ac2bf3e482f73bbf6a10a20bcbeb714aa
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-05-17 19:15:27 +0900

    Update recapchav2.js

--- recapchav2.js
+++ recapchav2.js
@@ -1,1 +1,24 @@
-
+function recapchajs() {		
+    var grecaptcharesponse = $('#g-recaptcha').val();
+    var secretAPIkey = $('#g-recapcha-key').val();
+      $.ajax({
+        type: 'POST',
+        url: "https://www.google.com/recaptcha/api/siteverify", 
+        data: {
+          secret:secretAPIkey,
+          response:grecaptcharesponse,
+        },
+        success: function( data ) {
+          data = JSON.parse(data);
+          if(data.success){
+            alert("success");
+          }else{
+            alert("faild");
+            return false;
+          }
+        },
+        error: function() {
+          alert("error");
+        }
+      });
+}
